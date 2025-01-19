b96b02f5a1b24322fe8733363301e28a1a7e228a
MrJokester <mrjokester01010@gmail.com>
2025-01-19 09:13:53 +0200

    Update MainButtons.js

--- components/MainButtons.js
+++ components/MainButtons.js
@@ -15,10 +15,10 @@ const Home = () => {
         <button className={styles.button} onClick={() => window.location.href = 'https://clarendonbot.vercel.app/cmds'}>
           <h2>Commands</h2>
         </button>
-        <button className={styles.button} onClick {() => window.location.href = 'https://clarendonbot.vercel.app/tos')>
+        <button className={styles.button} onClick {() => window.location.href = 'https://clarendonbot.vercel.app/tos'}>
           <h2>Terms Of Service</h2>
         </button>
-        <button className={styles.button} onClick {() => window.location.href = 'https://clarendonbot.vercel.app/privacy')>
+        <button className={styles.button} onClick {() => window.location.href = 'https://clarendonbot.vercel.app/privacy'}>
           <h2>Privacy Policy</h2>
         </button>
       </div>
