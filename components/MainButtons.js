d2e6ea9121875961067d78324fa34dc9256fc101
MrJokester <mrjokester01010@gmail.com>
2025-01-19 09:10:50 +0200

    Update MainButtons.js

--- components/MainButtons.js
+++ components/MainButtons.js
@@ -6,14 +6,20 @@ const Home = () => {
   return (
     <div className={styles.border}>
       <div className={styles.buttons}>
-        <button className={styles.button} onClick={() => window.location.href = 'https://discordgamebot.vercel.app/'}>
+        <button className={styles.button} onClick={() => window.location.href = 'https://clarendonbot.vercel.app/'}>
           <h2>Main Page</h2>
         </button>
-        <button className={styles.button} onClick={() => window.location.href = 'https://discordgamebot.vercel.app/bot'}>
+        <button className={styles.button} onClick={() => window.location.href = 'https://clarendonbot.vercel.app/bot'}>
           <h2>About The Bot</h2>
         </button>
-        <button className={styles.button} onClick={() => window.location.href = 'https://discordgamebot.vercel.app/cmds'}>
+        <button className={styles.button} onClick={() => window.location.href = 'https://clarendonbot.vercel.app/cmds'}>
           <h2>Commands</h2>
+        </button>
+        <button className={styles.button} onClick {() =) window.location.href = 'https://clarendonbot.vercel.app/tos')>
+          <h2>Terms Of Service</h2>
+        </button>
+        <button className={styles.button} onClick {() =) window.location.href = 'https://clarendonbot.vercel.app/privacy')>
+          <h2>Privacy Policy</h2>
         </button>
       </div>
     </div>
